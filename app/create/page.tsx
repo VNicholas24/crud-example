2f4bf719f8c427e15d09ecea2f96ad45a723d7f8
VNicholas24 <nichsebu@gmail.com>
2023-07-31 12:41:19 +0800

    first commit

---
+++ app/create/page.tsx
@@ -0,0 +1,11 @@
+import { FunctionComponent } from "react";
+
+interface CreatePageProps {
+    
+}
+ 
+const CreatePage: FunctionComponent<CreatePageProps> = () => {
+    return ( <></> );
+}
+ 
+export default CreatePage;
